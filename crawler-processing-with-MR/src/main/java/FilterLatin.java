086fed2eae6192f3aa5f5286ca49d7b8724237c5
Nelson Coquenim <n.coquenim@campus.fct.unl.pt>
2018-11-23 14:05:26 +0000

    Fixed problem in reducer of filter for latin words mapreduce job

--- crawler-processing-with-MR/src/main/java/FilterLatin.java
+++ crawler-processing-with-MR/src/main/java/FilterLatin.java
@@ -11,6 +11,7 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import sun.reflect.annotation.ExceptionProxy;
 import utils.*;
 
 import java.io.IOException;
@@ -28,26 +29,27 @@ public class FilterLatin {
             System.err.println(">>>Processing>>> " + ((FileSplit) cont.getInputSplit()).getPath().toString());
         }
 
-        public void map(LongWritable key, WritableWarcRecord value, Context cont) { //TODO is it better only map adding an attribute?
+        private boolean isLatinAlphabet(String text) {
+
+                return text.matches("[\\s\\p{L}\\p{M}&&[^\\p{Alpha}]]+");
+        }
+
+        public void map(LongWritable key, WritableWarcRecord value, Context cont) throws IOException { //TODO is it better only map adding an attribute?
 
             WarcRecord val = value.getRecord();
 
-            boolean isLatinAlphabet = true;
-
             String bytesString = val.getHeaderMetadataItem("Content-Length");
             String url = val.getHeaderMetadataItem("WARC-Target-URI");
-
+            String content = val.getContentUTF8();
             try {
-
-                if (isLatinAlphabet) {
+                if (content != null && isLatinAlphabet(content)) {
                     word.set(new URL(url).getHost());
-                    cont.write(word, new Text(bytesString + " " + new String(val.getContent())));
+                    cont.write(word, new Text(bytesString + " " + content));
                 }
             } catch (Exception e) {
-                System.err.println(url);
+                //throw new IOException("DEBUG_MAP : " + url + val.getContentUTF8().substring(0, 100));
             }
         }
-
     }
 
 
@@ -57,27 +59,21 @@ public class FilterLatin {
                 throws IOException, InterruptedException {
 
             int bytes = 0;
-            StringBuilder content = new StringBuilder("");
+            String content = "";
+
 
             for (Text val : values) {
-
-                try {
-                    StringTokenizer st = new StringTokenizer(val.toString());
-                    if (st.hasMoreTokens()) {
-                        bytes += Integer.getInteger(st.nextToken());
-                    }
-
-                    while (st.hasMoreTokens()) {
-                        content.append(" ");
-                        content.append(st.nextToken());
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    System.err.println("DEBUG = " + val.toString());
+                if (val != null) {
+                    String[] splitedVal = val.toString().split(" ", 2);
+                    if (splitedVal[1].length() < 10)
+                        content += (splitedVal[1].substring(0, (int) (splitedVal[1].length() * 0.10)));
+                    else
+                        content += (splitedVal[1].substring(0, 9));
+                    bytes += Integer.parseInt(splitedVal[0]);
+                    content += (" ");
                 }
-
             }
-            cont.write(key, new Text(String.valueOf(bytes) + ":" + content));
+            cont.write(key, new Text(String.valueOf(bytes) + " " + content));
         }
 
     }
