b9ddb3d20b010b979bcd975c245911b9d199f0e5
Nelson Coquenim <n.coquenim@campus.fct.unl.pt>
2018-11-26 12:57:17 +0000

    Minor tweaks to the test script

--- crawler-processing-with-MR/src/main/scripts/testQuery.sh
+++ crawler-processing-with-MR/src/main/scripts/testQuery.sh
@@ -3,19 +3,13 @@ outputDir=$1
 
 mkdir -p ./tests/test-results/$outputDir/
 
-for sampleSize in 1 #4 8 16
+for sampleSize in 1 4 8 16
 do
     for run in $(seq 1 3)
     do
-        yarn jar tests/crawler-processing-with-MR-1.0-SNAPSHOT.jar TopWordFrequency /tests/input-data/$sampleSize/* /tests/mr-results/$outputDir/t-$run >> tests/test-results/$outputDir/results-$sampleSize-temp.txt
-        && tail -n 1 tests/test-results/$outputDir/results-$sampleSize-temp.txt >> tests/test-results/$outputDir/results-$sampleSize.txt && rm tests/test-results/$outputDir/results-$sampleSize-temp.txt
+        echo "\n>>>Run " $run " with sample size " $sampleSize "has started.\n"
+        yarn jar tests/crawler-processing-with-MR-1.0-SNAPSHOT.jar TopWordFrequency /tests/input-data/$sampleSize/* /tests/mr-results/$outputDir/size-$sampleSize/run-$run >> tests/test-results/$outputDir/results-$sampleSize-temp.txt && tail -n 1 tests/test-results/$outputDir/results-$sampleSize-temp.txt >> tests/test-results/$outputDir/results-$sampleSize.txt && rm tests/test-results/$outputDir/results-$sampleSize-temp.txt
+        echo "\n>>>Run " $run " with sample size " $sampleSize "done.\n"
     done
 done
 echo "Tests concluded."
-
-#$sampleSize=$0
-#find /tests/input-data/* |head -$sampleSize
-
-# a) um ficheiro; b) quatro
-#ficheiros; c) oito ficheiros; d) 16 ficheiros.
-
