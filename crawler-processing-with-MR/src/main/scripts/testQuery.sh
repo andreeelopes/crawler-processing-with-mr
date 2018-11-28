f3364960a8da455629ffabbfa2402e728eb88cdf
Nelson Coquenim <n.coquenim@campus.fct.unl.pt>
2018-11-28 10:23:41 +0000

    Minor change to test scripts

--- crawler-processing-with-MR/src/main/scripts/testQuery.sh
+++ crawler-processing-with-MR/src/main/scripts/testQuery.sh
@@ -3,13 +3,13 @@ outputDir=$1
 
 mkdir -p ./tests/test-results/$outputDir/
 
-for sampleSize in 1 4 8 16
+for sampleSize in 1 4 8 16 32
 do
     for run in $(seq 1 3)
     do
-        echo "\n>>>Run " $run " with sample size " $sampleSize "has started.\n"
+        echo ">>>Run " $run " with sample size " $sampleSize "has started."
         yarn jar tests/crawler-processing-with-MR-1.0-SNAPSHOT.jar TopWordFrequency /tests/input-data/$sampleSize/* /tests/mr-results/$outputDir/size-$sampleSize/run-$run >> tests/test-results/$outputDir/results-$sampleSize-temp.txt && tail -n 1 tests/test-results/$outputDir/results-$sampleSize-temp.txt >> tests/test-results/$outputDir/results-$sampleSize.txt && rm tests/test-results/$outputDir/results-$sampleSize-temp.txt
-        echo "\n>>>Run " $run " with sample size " $sampleSize "done.\n"
+        echo ">>>Run " $run " with sample size " $sampleSize "done."
     done
 done
 echo "Tests concluded."
